9f3fec38ed056370f4dc56f88538bc27cadcf560
namaelsan <x.arybidx@gmail.com>
2024-12-25 18:40:26 +0300

    rclone working

--- hosts/default/services.nix
+++ hosts/default/services.nix
@@ -1,11 +1,27 @@
 { pkgs, ...}:
 
 {
-  systemd.services.rclone = {
-    description = "rclone command for mounting a dropbox remote to documents";
-    path = with pkgs; [ rclone ];
-    serviceConfig.PassEnvironment = "DISPLAY";
-    script = " mount.rclone dropbox: /home/namael/Documents/dropbox/";
+  # systemd.services.rclone = {
+  #   enable = true;
+  #   description = "rclone command for mounting a dropbox remote to documents";
+  #   path = with pkgs; [ rclone ];
+  #   serviceConfig.PassEnvironment = "DISPLAY";
+  #   script = " mount.rclone dropbox: /home/namael/Documents/dropbox/";
+
+  # };
+
+  systemd.services."rclone-sync" = {
+    enable = true;
+    description = "Rclone Sync Service";
+    wantedBy = ["default.target"]; # user service
+    path  = with pkgs; [ rclone getent ];
+    serviceConfig = {
+      RemainAfterExit = true;
+      WorkingDirectory = "/home/namael/";
+    };
+
+    script = "rclone mount dropbox: /home/namael/Documents/dropbox/ --config /home/namael/.config/rclone/rclone.conf";
 
   };
+
 }
