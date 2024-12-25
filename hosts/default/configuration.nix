2c4d08f2072406b613815cb6974966d2ba0ac862
namaelsan <x.arybidx@gmail.com>
2024-12-25 03:30:39 +0300

    required line

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -62,7 +62,8 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
+  services.displayManager.sddm.wayland.enable = true;
+  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "tr";
