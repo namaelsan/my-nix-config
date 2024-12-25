a627eaa8c29b3b6705ef66d3364a19c85cf48e87
namaelsan <x.arybidx@gmail.com>
2024-12-25 04:07:01 +0300

    test

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -29,6 +29,8 @@
     vesktop
     keepassxc
     git
+    steam
+    gamescope
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
