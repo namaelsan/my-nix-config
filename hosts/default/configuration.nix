aad3902ca3f18b0727f8f6ce6f078c7f924d0291
namaelsan <x.arybidx@gmail.com>
2024-12-25 03:54:54 +0300

    seems like i cant disable x11 for some reason

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -57,12 +57,11 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = false;
+  services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
   
   # Configure keymap in X11
   services.xserver.xkb = {
