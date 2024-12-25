97a85d859a167148a642175e6c7724314ef33f7b
namaelsan <x.arybidx@gmail.com>
2024-12-25 04:04:19 +0300

    steam&gamescope

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -43,6 +43,9 @@
     # '')
   ];
 
+  programs.steam.enable = true;
+  programs.gamescope.enable = true;
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
