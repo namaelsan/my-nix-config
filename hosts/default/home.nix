16dbf39cb7f89c3cfc874e91c6b4888e9b23d75d
namaelsan <x.arybidx@gmail.com>
2024-12-27 01:47:19 +0300

    androidstudio + fluttter

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -40,7 +40,9 @@
     universal-android-debloater
     telegram-desktop
     spotify
-    android-studio-full
+    android-studio # NOTE: android sdk was installed manually
+    vdhcoapp # video download helper coapp
+    flutter    
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
