c91b9f636694560f55214129c0743db905122160
namaelsan <x.arybidx@gmail.com>
2024-12-27 03:34:46 +0300

    sync igpu & gpu

--- hosts/default/nvidia.nix
+++ hosts/default/nvidia.nix
@@ -13,6 +13,7 @@
   hardware.nvidia = {
 
      prime = {
+      sync.enable = true;
       # Make sure to use the correct Bus ID values for your system!
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
