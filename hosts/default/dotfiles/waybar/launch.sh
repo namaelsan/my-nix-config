f6477cd5ec2b6b3cbe5601732af2327c42da356d
namaelsan <x.arybidx@gmail.com>
2024-12-28 23:13:36 +0300

    format

--- hosts/default/dotfiles/waybar/launch.sh
+++ hosts/default/dotfiles/waybar/launch.sh
@@ -2,5 +2,6 @@
 
 pkill waybar
 pkill swwww-daemon
+
 $HOME/nixos/hosts/default/dotfiles/waybar/scripts/background.sh
 waybar -c ~/nixos/hosts/default/dotfiles/waybar/config -s ~/nixos/hosts/default/dotfiles/waybar/style.css
