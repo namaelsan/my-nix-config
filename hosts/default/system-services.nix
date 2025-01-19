62317c9a6a9967182055b7df04177c058ce8e0fc
namaelsan <x.arybidx@gmail.com>
2025-01-19 04:11:26 +0300

    numlock service WORKING

--- hosts/default/system-services.nix
+++ hosts/default/system-services.nix
@@ -2,13 +2,22 @@
 
 {
   systemd.services.numLockOnTty = {
-    wantedBy = [ "default.target" ];
+    wantedBy = [ "display-manager.service" ];
+    path = [pkgs.dotool pkgs.coreutils];
     serviceConfig = {
+      Type = "simple";
       # /run/current-system/sw/bin/setleds -D +num < "$tty";
       # ExecStartPre = "${pkgs.coreutils}/bin/sleep 0.5"; # Delay of 0.5 seconds
       ExecStart = lib.mkForce (
-        pkgs.writeShellScript "numLockOnTty" ''
-          ${pkgs.coreutils}/bin/echo 'key x:Num_Lock' | ${pkgs.dotool}/bin/dotool
+        pkgs.writeShellScript "numLockOnTty7" ''
+          # Wait for SDDM to start (optional delay)
+          ${pkgs.coreutils}/bin/sleep 1.5
+
+          # Set the DISPLAY environment variable for tty7
+          export DISPLAY=":0"
+
+          # Use dotool to simulate the NumLock key press
+          echo key x:Num_Lock | dotool
         ''
       );
     };
