d492f3acddaf8cc474a84081180242b7488882c2
namaelsan <x.arybidx@gmail.com>
2025-03-07 21:40:50 +0300

    garbace collector

--- hosts/default/system-services.nix
+++ hosts/default/system-services.nix
@@ -22,4 +22,13 @@
       );
     };
   };
+
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    persistent = true;
+    options = "--delete-older-than 30d";
+  };
+  nix.settings.auto-optimise-store = true;
 }
