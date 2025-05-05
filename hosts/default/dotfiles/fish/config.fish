fbdd158a253757a2ac79c025c955657e2ef56f81
namaelsan <x.arybidx@gmail.com>
2025-05-05 10:37:58 +0300

    color theme(scrapped)

--- hosts/default/dotfiles/fish/config.fish
+++ hosts/default/dotfiles/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
+    # if test -e ~/.cache/wal/colors.fish
+    #     source ~/.cache/wal/colors.fish
+    # end
+
     fastfetch
 end
