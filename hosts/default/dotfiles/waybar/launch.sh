2ffb9848dad9715a64bae7b15de69d406577d00e
namaelsan <x.arybidx@gmail.com>
2025-05-05 10:40:24 +0300

    now sends notification

--- hosts/default/dotfiles/waybar/launch.sh
+++ hosts/default/dotfiles/waybar/launch.sh
@@ -4,3 +4,4 @@ pkill waybar
 
 $HOME/nixos/hosts/default/dotfiles/waybar/scripts/background.sh
 waybar -c ~/nixos/hosts/default/dotfiles/waybar/config -s ~/nixos/hosts/default/dotfiles/waybar/style.css
+notify-send -t 2500 "Waybar Launched"
