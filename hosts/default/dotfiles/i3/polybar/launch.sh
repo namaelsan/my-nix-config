a67df36351c7e5c10ea04426c6e48bb777e3e24d
namaelsan <x.arybidx@gmail.com>
2025-05-22 15:12:38 +0300

    polybar with pywal

---
+++ hosts/default/dotfiles/i3/polybar/launch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Paths
+WALLPAPER_DIR="$HOME/Wallpapers"  # Change to your wallpapers folder
+SELECTED_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)  # Select random wallpaper
+POLYBAR_CONFIG="$HOME/nixos/hosts/default/dotfiles/i3/polybar/config"
+
+# Set the wallpaper
+feh --bg-scale --zoom fill $SELECTED_WALLPAPER
+
+# Generate colors using pywal
+wal -n -e --cols16 darken -i "$SELECTED_WALLPAPER" 
+cp $SELECTED_WALLPAPER ~/.cache/current_wp
+xrdb -merge $HOME/.cache/wal/colors.Xresources
+i3-msg reload
+
+# Launch mybar
+echo "---" | tee -a /tmp/polybar1.log
+
+MONITOR=HDMI-0 polybar mybar --config=$POLYBAR_CONFIG 2>&1 | tee -a /tmp/polybar1.log & disown &
+MONITOR=eDP-1-1 polybar secondary --config=$POLYBAR_CONFIG 2>&1 | tee -a /tmp/polybar2.log & disown &
+
+echo "Bars launched..."
