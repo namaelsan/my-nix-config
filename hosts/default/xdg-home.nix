4fa3e89dcaf2b9bb258fcd9fe0e5d10e1d5cdc7b
namaelsan <x.arybidx@gmail.com>
2025-05-22 14:46:38 +0300

    default apps for home-manager

---
+++ hosts/default/xdg-home.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+
+{
+
+  # Set default applications
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # "" = [ "" ];
+
+        # Web browser
+        "text/html" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+
+        # Compressed files
+        "application/zip" = [ "ark.desktop" ];
+        "application/vnd.rar" = [ "ark.desktop" ];
+
+        # Torrent
+        "application/x-bittorrent" = [ "deluge.desktop" ];
+
+        # Document
+        "application/vnd.oasis.opendocument.txt" = [ "writer.desktop" ];
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ "impress" ];
+        "application/vnd.ms-excel" = [ "calc.desktop" ];
+
+
+        # PDF viewer
+        "application/pdf" = [ "zen-beta.desktop" ];
+
+        # Image viewer
+        "image/png" = [ "swayimg.desktop" ];
+        "image/jpeg" = [ "swayimg.desktop" ];
+        "image/webp" = [ "swayimg.desktop" ];
+        "image/svg+xml" = [ "swayimg.desktop" ];
+
+        # Video player
+        "video/mp4" = [ "mpv.desktop" ];
+        "video/x-matroska" = [ "mpv.desktop" ];
+        "application/vnd.apple.mpegurl" = [ "mpv.desktop" ]; # m3u8 files
+
+        # Email client
+        # "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
+      };
+    };
+  };
+}
