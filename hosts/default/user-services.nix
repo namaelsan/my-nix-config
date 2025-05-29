e78030e22910154c00b544eb13cb24880dc912e7
namaelsan <x.arybidx@gmail.com>
2025-05-29 22:10:26 +0300

    rclonesync retry on failure

--- hosts/default/user-services.nix
+++ hosts/default/user-services.nix
@@ -8,6 +8,9 @@
         KillSignal = "SIGTERM"; # Sends SIGTERM when stopping or killing the service
         KillMode = "mixed"; # Ensures the signal propagates to child processes
         ExecStart = "${pkgs.rclone}/bin/rclone mount dropbox: /home/namael/Documents/dropbox/ --config /home/namael/.config/rclone/rclone.conf";
+        # Retry every 5 seconds if the mount fails
+        Restart = "on-failure";
+        RestartSec = "5s";
       };
       Unit = {
         Description = "Rclone Sync Service"; # Set the description here
