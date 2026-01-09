27029a0568d448cb668463a46c17d82f15c75c96
namaelsan <x.arybidx@gmail.com>
2026-01-09 21:19:55 +0300

    added blueman

--- dotfiles/hyprland/hyprstart.sh
+++ dotfiles/hyprland/hyprstart.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # run clipboard listener on startup
-clipse -listen 
+clipse -listen &
 # # wallpaper
 swww-daemon &
 # swww img ~/Wallpapers/wp9223560-4k-night-hd-wallpapers.jpg & ## unnecessary launch.sh launches background.sh
 # network applet
 nm-applet --indicator & 
-mako 
+blueman-applet &
+mako &
