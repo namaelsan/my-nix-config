56ab664792eefb805706680ad9f0ac20b2ceb755
namaelsan <x.arybidx@gmail.com>
2026-01-12 16:19:01 +0300

    changed to fuzzel

--- dotfiles/waybar/scripts/applauncher.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# pkill rofi; 
-# sleep 0.1; 
-rofi -show drun -show-icons -theme ~/.cache/wal/colors-rofi-dark.rasi -replace
---
+++ hosts/default/scripts/applauncher.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# pkill rofi; 
+# sleep 0.1; 
+# rofi -show drun -show-icons -theme ~/.cache/wal/colors-rofi-dark.rasi -replace
+fuzzel
