7f93ccc68372e5b7a89ec951be9ffc1418a2d93b
namaelsan <x.arybidx@gmail.com>
2026-02-03 19:55:20 +0300

    fix getting stuck in startup

--- dotfiles/hyprland/hyprstart.sh
+++ dotfiles/hyprland/hyprstart.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # run clipboard listener on startup
-clipse -listen 
+clipse -listen
 # # wallpaper
-swww-daemon 
+swww-daemon &
 # swww img ~/Wallpapers/wp9223560-4k-night-hd-wallpapers.jpg & ## unnecessary launch.sh launches background.sh
 # network applet
-nm-applet --indicator  
+nm-applet --indicator &
 systemctl --user start hyprpolkitagent 
-blueman-applet 
-qbittorrent 
-mako 
+blueman-applet &
+qbittorrent &
+mako &
