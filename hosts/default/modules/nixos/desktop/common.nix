4818ccb25ffb095609ff6c32308a2a2eaa58bc5c
namaelsan <x.arybidx@gmail.com>
2026-02-26 18:38:21 +0300

    unnecessary because xdg.nix exists

--- hosts/default/modules/nixos/desktop/common.nix
+++ hosts/default/modules/nixos/desktop/common.nix
@@ -1,16 +1,6 @@
 { ... }:
 
 {
-  # Communication between apps
-  services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    # config.common.default = [ "hyprland" ];
-    # Ensure these packages are installed
-    # extraPortals = [ pkgs.xdg-desktop-portal-hyprland ]; # Good fallback/compatibility
-    # wlr.enable = true; # Explicitly enable the wlroots-based portal if needed
-  };
-
   # Polkit for authentication
   security.polkit.enable = true;
 
